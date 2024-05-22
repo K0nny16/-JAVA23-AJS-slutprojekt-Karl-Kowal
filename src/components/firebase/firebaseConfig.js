582ba49d57f93bcbcdc441bc7c41db685bfab37e
Karl Kowal <kowal.karl99@gmail.com>
2024-05-22 12:12:02 +0200

    fixade firebase fel

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase,ref } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARXU3MK-USTUKTLo7aBqVy-9WzNcD3l-Y",
-    authDomain: "scrum-board-7e3d2.firebaseapp.com",
-    projectId: "scrum-board-7e3d2",
-    storageBucket: "scrum-board-7e3d2.appspot.com",
-    messagingSenderId: "476196483267",
-    appId: "1:476196483267:web:b87763c49315d074a03276"
-}
+  apiKey: "AIzaSyARXU3MK-USTUKTLo7aBqVy-9WzNcD3l-Y",
+  authDomain: "scrum-board-7e3d2.firebaseapp.com",
+  databaseURL: "https://scrum-board-7e3d2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "scrum-board-7e3d2",
+  storageBucket: "scrum-board-7e3d2.appspot.com",
+  messagingSenderId: "476196483267",
+  appId: "1:476196483267:web:b87763c49315d074a03276"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
