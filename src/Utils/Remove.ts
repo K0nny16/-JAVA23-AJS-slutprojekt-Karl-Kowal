91042257a2ea0ce8b0e72fb662f0962a65b4d6f5
Karl Kowal <kowal.karl99@gmail.com>
2024-05-27 13:50:43 +0200

    fixade felhantering ifall man inte kommer åt FB

--- src/Utils/Remove.ts
+++ src/Utils/Remove.ts
@@ -2,6 +2,11 @@ import { ref, remove } from "firebase/database";
 import { db } from "../firebase/firebaseConfig.js";
 //Tarbort en användare från en viss tabbel.
 export function removeTask(key:string,table:string):void{
-    const userToRemove = ref(db,table+key);
-    remove(userToRemove);
+    try{
+        const userToRemove = ref(db,table+key);
+        remove(userToRemove);
+    }catch(error){
+        alert("Något gick fel testa igen!");
+        console.log(error);
+    }
 }
